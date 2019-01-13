269c668a8e5345528b6e2de434fdd11ae55a528c
Philipp Jardas <philipp@jardas.de>
2019-01-13 16:01:10 +0100

    Improve accessibility

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,19 @@ export default function IndexPage() {
   return (
     <div className="page">
       <SEO
+        lang="de"
         title="Main-Kinzig-Nerds"
         description="Das Netzwerk für Software People im Main-Kinzig-Kreis"
         keywords={['meetup', 'mkk', 'main-kinzig-kreis']}
       />
-      <LogoLarge
-        src="logo-large.png"
-        alt="Main-Kinzig-Nerds"
-        className="logo"
-      />
-      <a href="https://www.meetup.com/Main-Kinzig-Nerds/" className="cta">
-        Besuche unsere Meetup-Gruppe
-      </a>
+      <h1>
+        <LogoLarge src="logo-large.png" alt="Main-Kinzig-Nerds" />
+      </h1>
+      <main>
+        <a href="https://www.meetup.com/Main-Kinzig-Nerds/" className="cta">
+          Besuche unsere Meetup-Gruppe
+        </a>
+      </main>
     </div>
   )
 }
